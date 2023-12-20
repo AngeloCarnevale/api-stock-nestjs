c207b16c1ccaad05a2101b5d1133ba34e9fe0503
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2023-12-20 11:22:08 -0300

    new: stock-input resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { StockInputModule } from './stock-input/stock-input.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, StockInputModule],
   controllers: [AppController],
   providers: [AppService],
 })
